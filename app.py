aaf84082865d2621a571dd6f6363945402ace1a7
Alan <alan@alanklas.com>
2023-12-21 20:31:33 +0100

    fixed generation of random strings & added if __name__ == __main__

--- app.py
+++ app.py
@@ -1,53 +1,61 @@
 import requests
 import json
 import time
+import random
+import string
 
 # ---
 # Coded by Odd!
+# Updated by DuckyBlender
 # Github: https://github.com/OddDevelopment/OperaGX-Nitro-Gen
 # Portfolio: https://odd.gay
 # ---
 
-url = 'https://api.discord.gx.games/v1/direct-fulfillment'
-headers = {
-    'authority': 'api.discord.gx.games',
-    'accept': '*/*',
-    'accept-language': 'en-US,en;q=0.9',
-    'content-type': 'application/json',
-    'origin': 'https://www.opera.com',
-    'referer': 'https://www.opera.com/',
-    'sec-ch-ua': '"Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'cross-site',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0'
-}
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choices(characters, k=length))
 
-data = {
-    'partnerUserId': '50b1bf177eca2a06f77680c1aa6277e1d5a44eb6d8b4a72545348e4828cf0753'
-}
+if __name__ == "__main__":
+    url = 'https://api.discord.gx.games/v1/direct-fulfillment'
+    headers = {
+        'authority': 'api.discord.gx.games',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        'origin': 'https://www.opera.com',
+        'referer': 'https://www.opera.com/',
+        'sec-ch-ua': '"Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0'
+    }
 
-session = requests.Session()
+    data = {
+        'partnerUserId': generate_random_string(64)
+    }
 
-try:
-    while True:
-        response = session.post(url, headers=headers, json=data)
+    session = requests.Session()
 
-        if response.status_code == 200:
-            token = response.json()['token']
-            with open('codes.txt', 'a') as file:
-                file.write(f"https://discord.com/billing/partner-promotions/1180231712274387115/{token}\n")
-            print("Token saved to codes.txt file.")
-        else:
-            print(f"Request failed with status code {response.status_code}.")
-            print(f"Error message: {response.text}")
+    try:
+        while True:
+            response = session.post(url, headers=headers, json=data)
 
-        time.sleep(1)
+            if response.status_code == 200:
+                token = response.json()['token']
+                with open('codes.txt', 'a') as file:
+                    file.write(f"https://discord.com/billing/partner-promotions/1180231712274387115/{token}\n")
+                print("Token saved to codes.txt file.")
+            else:
+                print(f"Request failed with status code {response.status_code}.")
+                print(f"Error message: {response.text}")
 
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+            time.sleep(1)
 
-finally:
-    session.close()
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+    finally:
+        session.close()
