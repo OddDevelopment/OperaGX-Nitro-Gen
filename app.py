186411496b31f4cb066625ecc0606691f0425493
OddDevelopment <135460135+OddDevelopment@users.noreply.github.com>
2024-03-02 16:48:08 -0800

    Fixed gen

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import requests
-import json
 import time
-import random
-import string
 
 # ---
 # Coded by Odd!
@@ -11,57 +8,56 @@ import string
 # Portfolio: https://odd.rip
 # ---
 
-def generate_random_string(length):
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choices(characters, k=length))
+def get_token():
+  url = "https://api.gx.me/profile/token"
+  headers = {
+      'accept':
+      'application/json',
+      'accept-language':
+      'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+      'authority':
+      'api.gx.me',
+      'cookie':
+      'SESSION_TYPE=user; SESSION=NzFjMjg3NDAtMDhkOC00ODkwLWJhNzEtODA0YTcwMjNiM2U0',
+      'origin':
+      'https://www.opera.com',
+      'referer':
+      'https://www.opera.com/',
+      'sec-ch-ua':
+      '"Not A(Brand";v="99", "Opera GX";v="107", "Chromium";v="121"',
+      'sec-ch-ua-mobile':
+      '?0',
+      'sec-ch-ua-platform':
+      '"Windows"',
+      'sec-fetch-dest':
+      'empty',
+      'sec-fetch-mode':
+      'cors',
+      'sec-fetch-site':
+      'cross-site',
+      'user-agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0',
+  }
+
+  response = requests.get(url, headers=headers)
+  if response.status_code == 200:
+    data = response.json()
+    return data['data']
+  else:
+    print("Failed to retrieve token")
+    return None
+
+
+def main():
+  while True:
+    token = get_token()
+    if token:
+      new_url = f"https://discord.com/billing/partner-promotions/1180231712274387115/{token}"
+      print(new_url)
+      with open("token_url.txt", "a") as file:
+        file.write(new_url + "\n")
+    time.sleep(0.5)
+
 
 if __name__ == "__main__":
-    url = 'https://api.discord.gx.games/v1/direct-fulfillment'
-    headers = {
-        'authority': 'api.discord.gx.games',
-        'accept': '*/*',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/json',
-        'origin': 'https://www.opera.com',
-        'referer': 'https://www.opera.com/',
-        'sec-ch-ua': '"Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0'
-    }
-
-    data = {
-        'partnerUserId': generate_random_string(64)
-    }
-
-    session = requests.Session()
-
-    try:
-        while True:
-            response = session.post(url, headers=headers, json=data)
-
-            if response.status_code == 200:
-                token = response.json()['token']
-                with open('codes.txt', 'a') as file:
-                    file.write(f"https://discord.com/billing/partner-promotions/1180231712274387115/{token}\n")
-                print("Token saved to codes.txt file.")
-            elif response.status_code == 429:
-                print("Rate limit exceeded! Waiting one minute to allow for cooldown.")
-                time.sleep(60)
-            elif response.status_code == 504:
-                print("Server timed out! Trying again in 5 seconds.")
-                time.sleep(5)
-            else:
-                print(f"Request failed with status code {response.status_code}.")
-                print(f"Error message: {response.text}")
-
-            time.sleep(1)
-
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-    finally:
-        session.close()
+  main()
