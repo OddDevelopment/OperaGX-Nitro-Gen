ec4ab4b5cbfcdd23ccfcbe46d5d1b60c24348e7f
Kayden <160945338+DarkTurns14@users.noreply.github.com>
2024-03-09 22:52:04 -0500

    Formatted + Semi-Fix generator

--- app.py
+++ app.py
@@ -1,65 +1,66 @@
 import requests
 import time
+import random
 
 # ---
 # Coded by Odd!
 # Contributers: DuckyBlender and NorthernChicken
 # Github: https://github.com/OddDevelopment/OperaGX-Nitro-Gen
 # Portfolio: https://odd.rip
+# Formatted and Semi-Fixed by Dark_Turns. Odd please reach out to me discord: dark_turns
 # ---
 
-print("Our project is https://github.com/OddDevelopment/OperaGX-Nitro-Gen please give it a star to support the project <3.\n\nMore about me: https://odd.rip\n\n\n")
+print(
+    "Our project is https://github.com/OddDevelopment/OperaGX-Nitro-Gen please give it a star to support the project <3.\n\nMore about me: https://odd.rip\n\n\n"
+)
+# This is to fix the same session type bug.
+# Though I am not sure how to fix "Uh Oh! Looks like something went wrong."
+
+# List of cookies to randomly choose from
+cookies = [
+    "SESSION_TYPE=user; SESSION=NzFjMjg3NDAtMDhkOC00ODkwLWJhNzEtODA0YTcwMjNiM2U0",
+    # Add more cookies here!
+]
+
 
 def get_token():
-  url = "https://api.gx.me/profile/token"
-  headers = {
-      'accept':
-      'application/json',
-      'accept-language':
-      'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
-      'authority':
-      'api.gx.me',
-      'cookie':
-      'SESSION_TYPE=user; SESSION=NzFjMjg3NDAtMDhkOC00ODkwLWJhNzEtODA0YTcwMjNiM2U0',
-      'origin':
-      'https://www.opera.com',
-      'referer':
-      'https://www.opera.com/',
-      'sec-ch-ua':
-      '"Not A(Brand";v="99", "Opera GX";v="107", "Chromium";v="121"',
-      'sec-ch-ua-mobile':
-      '?0',
-      'sec-ch-ua-platform':
-      '"Windows"',
-      'sec-fetch-dest':
-      'empty',
-      'sec-fetch-mode':
-      'cors',
-      'sec-fetch-site':
-      'cross-site',
-      'user-agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0',
-  }
+    url = "https://api.gx.me/profile/token"
+    headers = {
+        "accept": "application/json",
+        "accept-language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+        "authority": "api.gx.me",
+        # Randomly selects cookies.
+        "cookie": random.choice(cookies),
+        "origin": "https://www.opera.com",
+        "referer": "https://www.opera.com/",
+        "sec-ch-ua": '"Not A(Brand";v="99", "Opera GX";v="107", "Chromium";v="121"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0",
+    }
 
-  response = requests.get(url, headers=headers)
-  if response.status_code == 200:
-    data = response.json()
-    return data['data']
-  else:
-    print("Failed to retrieve token")
-    return None
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]
+    else:
+        print("Failed to retrieve token")
+        return None
 
 
 def main():
-  while True:
-    token = get_token()
-    if token:
-      new_url = f"https://discord.com/billing/partner-promotions/1180231712274387115/{token}"
-      print(f"{new_url}\n\n")
-      with open("token_url.txt", "a") as file:
-        file.write(new_url + "\n")
-    time.sleep(0.5)
+    while True:
+        token = get_token()
+        if token:
+            new_url = f"https://discord.com/billing/partner-promotions/1180231712274387115/{token}"
+            print(f"{new_url}\n\n")
+            with open("token_url.txt", "a") as file:
+                file.write(new_url + "\n")
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
-  main()
+    main()
